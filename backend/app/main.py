636901bbe1e128893889f8b5e6c5500eafe3231c
Opahgrammer <gamingfajar438@gmail.com>
2025-12-12 18:04:27 +0700

    Fix CORS allow all origins for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,10 +25,10 @@ from .disease_detection import detector
 
 app = FastAPI(title="TomatoCare API", version="1.0.0")
 
-# --- KONFIGURASI CORS ---
+# --- KONFIGURASI CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
